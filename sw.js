237b7ce343ec51fa4c1f5eade537c6f5ccef2838
mihasusnik112-collab <mihasusnik112@gmail.com>
2026-08-10 15:06:47 +0200

    Dodana moderna ikona tekača ter PWA namestitev aplikacije na telefon ali tablico

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'treadmill-pro-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './treadmill.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
